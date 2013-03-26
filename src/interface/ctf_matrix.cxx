c930b3b6d2894cf393eed48319c3a244da07333f
Devin Matthews <dmatthews@utexas.edu>
2013-03-26 18:15:19 -0500

    Fix to avoid (int[]){...}

--- src/interface/ctf_matrix.cxx
+++ src/interface/ctf_matrix.cxx
@@ -1,11 +1,25 @@
 #include <ctf.hpp>
 
+struct int2
+{
+	int i[2];
+	int2(int a, int b)
+	{
+		i[0] = a;
+		i[1] = b;
+	}
+	operator const int*() const
+	{
+		return i;
+	}
+};
+
 template<typename dtype>
 tCTF_Matrix<dtype>::tCTF_Matrix(int const           nrow_,
                                 int const           ncol_,
                                 int const           sym_,
                                 tCTF_World<dtype> * world) :
-  tCTF_Tensor<dtype>(2, (int[]){nrow_, ncol_}, (int[]){sym_, NS}, world) {
+  tCTF_Tensor<dtype>(2, int2(nrow_, ncol_), int2(sym_, NS), world) {
   nrow = nrow_;
   ncol = ncol_;
   sym = sym_;
--- src/interface/ctf_vector.cxx
+++ src/interface/ctf_vector.cxx
@@ -1,9 +1,22 @@
 #include <ctf.hpp>
 
+struct int1
+{
+	int i[0];
+	int1(int a)
+	{
+		i[0] = a;
+	}
+	operator const int*() const
+	{
+		return i;
+	}
+};
+
 template<typename dtype>
 tCTF_Vector<dtype>::tCTF_Vector(int const           len_,
                                 tCTF_World<dtype> * world) :
-  tCTF_Tensor<dtype>(1, (int[]){len_}, (int[]){NS}, world) {
+  tCTF_Tensor<dtype>(1, int1(len_), int1(NS), world) {
   len = len_;
   
 }
