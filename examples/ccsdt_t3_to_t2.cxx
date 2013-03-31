ee39053cdcf477b3f7298c39c0e6ab2626aa0174
Edgar Solomonik <solomon@eecs.berkeley.edu>
2013-03-30 22:42:11 -0700

    Added correctness to ccsdt test, turned off desymmetrization

--- examples/ccsdt_t3_to_t2.cxx
+++ examples/ccsdt_t3_to_t2.cxx
@@ -11,14 +11,11 @@
 #include "../src/shared/util.h"
 
 void ccsdt_t3_to_t2(int const  n,
-                    int const  sym,
-                    int const  niter,
                     CTF_World  &dw,
                     char const *dir){
   int rank, i, num_pes;
   int64_t np;
   double * pairs;
-  double t, time;
   int64_t * indices;
   
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -27,56 +24,71 @@ void ccsdt_t3_to_t2(int const  n,
   if (rank == 0)
     printf("n = %d\n", n);
   
-  int shapeN4[] = {sym,NS,sym,NS};
-  int shapeN6[] = {sym,sym,NS,sym,sym,NS};
+  int shapeAS4[] = {AS,NS,AS,NS};
+  int shapeAS6[] = {AS,AS,NS,AS,AS,NS};
+  int shapeNS4[] = {NS,NS,NS,NS};
+  int shapeNS6[] = {NS,NS,NS,NS,NS,NS};
   int sizeN4[] = {n,n,n,n};
   int sizeN6[] = {n,n,n,n,n,n};
 
   //* Creates distributed tensors initialized with zeros
-  CTF_Tensor A(4, sizeN4, shapeN4, dw);
-  CTF_Tensor B(6, sizeN6, shapeN6, dw);
-  CTF_Tensor C(4, sizeN4, shapeN4, dw);
+  CTF_Tensor AS_A(4, sizeN4, shapeAS4, dw);
+  CTF_Tensor AS_B(6, sizeN6, shapeAS6, dw);
+  CTF_Tensor AS_C(4, sizeN4, shapeAS4, dw);
+  CTF_Tensor NS_A(4, sizeN4, shapeNS4, dw);
+  CTF_Tensor NS_B(6, sizeN6, shapeNS6, dw);
+  CTF_Tensor NS_C(4, sizeN4, shapeNS4, dw);
 
   if (rank == 0)
     printf("tensor creation succeed\n");
 
   //* Writes noise to local data based on global index
-  A.get_local_data(&np, &indices, &pairs);
+  AS_A.get_local_data(&np, &indices, &pairs);
   for (i=0; i<np; i++ ) pairs[i] = (1.E-3)*sin(indices[i]);
-  A.write_remote_data(np, indices, pairs);
+  AS_A.write_remote_data(np, indices, pairs);
+  NS_A.write_remote_data(np, indices, pairs);
   free(pairs);
   free(indices);
-  B.get_local_data(&np, &indices, &pairs);
+  AS_B.get_local_data(&np, &indices, &pairs);
   for (i=0; i<np; i++ ) pairs[i] = (1.E-3)*sin(.33+indices[i]);
-  B.write_remote_data(np, indices, pairs);
+  AS_B.write_remote_data(np, indices, pairs);
+  AS_B.write_remote_data(np, indices, pairs);
   free(pairs);
   free(indices);
-  C.get_local_data(&np, &indices, &pairs);
+  AS_C.get_local_data(&np, &indices, &pairs);
   for (i=0; i<np; i++ ) pairs[i] = (1.E-3)*sin(.66+indices[i]);
-  C.write_remote_data(np, indices, pairs);
+  AS_C.write_remote_data(np, indices, pairs);
+  NS_C.write_remote_data(np, indices, pairs);
+
+  AS_C["ijmn"] += .5*AS_A["mnje"]*AS_B["abeimn"];
+
+  NS_C["ijmn"] += NS_A["mnje"]*NS_B["abeimn"];
+  NS_C["ijmn"] -= NS_A["mnje"]*NS_B["abemin"];
+  NS_C["ijmn"] -= NS_A["mnje"]*NS_B["abenmi"];
+  NS_C["ijmn"] -= NS_A["mnje"]*NS_B["aebimn"];
+  NS_C["ijmn"] += NS_A["mnje"]*NS_B["aebmin"];
+  NS_C["ijmn"] += NS_A["mnje"]*NS_B["aebnmi"];
+  NS_C["ijmn"] -= NS_A["mnje"]*NS_B["eabimn"];
+  NS_C["ijmn"] += NS_A["mnje"]*NS_B["eabmin"];
+  NS_C["ijmn"] += NS_A["mnje"]*NS_B["eabnmi"];
+  NS_C["ijmn"] -= NS_A["mnej"]*NS_B["abeimn"];
+  NS_C["ijmn"] += NS_A["mnej"]*NS_B["abemin"];
+  NS_C["ijmn"] += NS_A["mnej"]*NS_B["abenmi"];
+  NS_C["ijmn"] += NS_A["mnej"]*NS_B["aebimn"];
+  NS_C["ijmn"] -= NS_A["mnej"]*NS_B["aebmin"];
+  NS_C["ijmn"] -= NS_A["mnej"]*NS_B["aebnmi"];
+  NS_C["ijmn"] += NS_A["mnej"]*NS_B["eabimn"];
+  NS_C["ijmn"] -= NS_A["mnej"]*NS_B["eabmin"];
+  NS_C["ijmn"] -= NS_A["mnej"]*NS_B["eabnmi"];
+  
+  NS_C.get_remote_data(np, indices, pairs);
+  AS_C.write_remote_data(np, indices, pairs);
+
+  double nrm = AS_C.reduce(CTF_OP_SQNRM2);
+  if (rank == 0) printf("norm of AS_C after contraction should be zero, is = %lf\n", nrm);
+
   free(pairs);
   free(indices);
-
-  C["ijmn"] += (.5*i)*A["mnje"]*B["abeimn"];
-  double nrm = C.reduce(CTF_OP_SQNRM2);
-  if (rank == 0) printf("norm of C after contraction is = %lf\n", nrm);
-
-  t = MPI_Wtime();
-  for (i=0; i<niter; i++){
-    C["ijmn"] += (.5*i)*A["mnje"]*B["abeimn"];
-  }
-
-  time = MPI_Wtime()- t;
-  if (rank == 0){
-    double nd = (double)n;
-    double c = 2.E-9;
-    if (sym == SY || sym == AS){
-      c = c/2.;
-    }
-    printf("%lf seconds/contrction %lf GF\n",
-            time/niter,niter*c*nd*nd*nd*nd*nd*nd*nd*nd/time);
-  }
-  
 } 
 
 
@@ -115,15 +127,7 @@ int main(int argc, char ** argv){
 
   CTF_World dw;
 
-  if (rank == 0){
-    printf("Computing C_ijmn = A_mnje*B_abeimn\n");
-    printf("Non-symmetric: NS = NS*NS:\n");
-  }
-  ccsdt_t3_to_t2(n, NS, niter, dw, dir);
-  if (rank == 0){
-    printf("(Anti-)Skew-symmetric: AS = AS*AS:\n");
-  }
-  ccsdt_t3_to_t2(n, AS, niter, dw, dir);
+  ccsdt_t3_to_t2(n, dw, dir);
 
 
   MPI_Finalize();
