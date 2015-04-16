01153acecf71907ee7491c072f46a87fe85ec000
Edgar Solomonik <solomonik@inf.ethz.ch>
2015-04-16 07:07:02 -0700

    no use fompi

--- test/test_suite.cxx
+++ test/test_suite.cxx
@@ -43,11 +43,7 @@ int main(int argc, char ** argv){
 
   int nt;
   //MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &nt);
-#ifdef USE_FOMPI
   foMPI_Init(&argc, &argv);
-#else
-  MPI_Init(&argc, &argv);
-#endif
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
 
