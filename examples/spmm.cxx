e4220e6587ccbf2efb365c0cd4cf6157a2ad2966
Edgar Solomonik <solomonik@inf.ethz.ch>
2015-08-25 18:02:08 +0200

    Reverted spmm example to doing two equivalent versions of SPMV

--- examples/spmm.cxx
+++ examples/spmm.cxx
@@ -38,8 +38,8 @@ int spmm(int     n,
   
   c1["ik"] += dnA["ij"]*b["jk"];
   
-  c2["ik"] += spA["ij"]*b["jk"];
-  //c2["ik"] += 0.5*b["jk"]*spA["ij"];
+  c2["ik"] += 0.5*spA["ij"]*b["jk"];
+  c2["ik"] += 0.5*b["jk"]*spA["ij"];
 
   /*if (dw.rank == 0) printf("b\n");
   b.print();
