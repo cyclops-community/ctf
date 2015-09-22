d184af64eedfccddb9006547f67c861f309b77a6
Edgar Solomonik <solomonik@inf.ethz.ch>
2015-09-22 13:31:12 +0200

    insignificant update to model parameters

--- src/shared/init_models.cxx
+++ src/shared/init_models.cxx
@@ -1,17 +1,15 @@
 namespace CTF_int{
-
-  double seq_tsr_ctr_mdl_cst_init[] = {3.8791E-13, 5.1204E-09, 4.2670E-10};
-  double seq_tsr_ctr_mdl_ref_init[] = {1.2049E-16, 8.7478E-10, 7.2899E-11};
-  double seq_tsr_ctr_mdl_inr_init[] = {1.1033E-04, 1.8379E-10, 9.9426E-11};
-  double long_contig_transp_mdl_init[] = {-5.8387E-04, 7.8748E-09};
-  double shrt_contig_transp_mdl_init[] = {5.5768E-03, 1.7220E-08};
-  double non_contig_transp_mdl_init[] = {-7.3027E-04, 1.1653E-08};
-  double alltoall_mdl_init[] = {1.0000E-06, 1.0000E-06, 5.0000E-10};
-  double alltoallv_mdl_init[] = {3.4237E-06, 8.8502E-06, 4.9445E-10};
-  double allred_mdl_init[] = {-6.0909E-05, 3.4977E-05, 1.1986E-09, 1.8655E-10};
-  double bcast_mdl_init[] = {2.4442E-04, -6.8112E-05, 9.4617E-11, 3.8892E-10};
-  double dgtog_res_mdl_init[] = {6.7192E-05, 1.7369E-04, 8.7337E-10};
-  double blres_mdl_init[] = {-1.6717E-06, -4.3213E-06, 3.1592E-10};
-
+double seq_tsr_ctr_mdl_cst_init[] = {3.8587E-13, 5.0935E-09, 4.2446E-10};
+double seq_tsr_ctr_mdl_ref_init[] = {1.3531E-16, 9.8233E-10, 8.1860E-11};
+double seq_tsr_ctr_mdl_inr_init[] = {2.3789E-04, 1.8066E-10, 1.2628E-10};
+double long_contig_transp_mdl_init[] = {-5.4894E-04, 8.3852E-09};
+double shrt_contig_transp_mdl_init[] = {5.5768E-03, 1.7220E-08};
+double non_contig_transp_mdl_init[] = {-7.2642E-04, 1.2788E-08};
+double alltoall_mdl_init[] = {1.0000E-06, 1.0000E-06, 5.0000E-10};
+double alltoallv_mdl_init[] = {5.7535E-05, 1.4873E-04, 6.1988E-10};
+double allred_mdl_init[] = {-3.5645E-05, -1.6542E-05, 2.7085E-10, 1.6774E-09};
+double bcast_mdl_init[] = {4.1375E-04, -1.3833E-04, 3.9615E-10, 3.0845E-10};
+double dgtog_res_mdl_init[] = {1.7143E-04, 4.4315E-04, 9.3862E-10};
+double blres_mdl_init[] = {-1.7690E-05, -4.5729E-05, 3.7256E-10};
 }
 
