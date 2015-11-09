aebf0232e8536c615d137bb2e5c75a17969811e9
Edgar Solomonik <solomonik@inf.ethz.ch>
2015-11-09 05:09:10 -0800

    New model parameters based on 400 sec training run on 384 cores of Edison with 6 processes per node. They seem to do well for AQ CCSD/CCSDT with flat MPI or threads on 384 cores.

--- src/shared/init_models.cxx
+++ src/shared/init_models.cxx
@@ -1,16 +1,16 @@
 namespace CTF_int{
-double blres_mdl_init[] = {1.2880E-05, 1.1099E-08}; 
-double long_contig_transp_mdl_init[] = {9.1201E-15, 2.1816E-09};                                         
-double shrt_contig_transp_mdl_init[] = {5.5768E-03, 1.7220E-08};                                         
-double non_contig_transp_mdl_init[] = {1.0753E-06, 4.1534E-08};
-double dgtog_res_mdl_init[] = {-8.1392E-04, 1.2781E-04, 1.2295E-09};
-double seq_tsr_ctr_mdl_cst_init[] = {3.8587E-13, 5.0935E-09, 4.2446E-10};
-double seq_tsr_ctr_mdl_ref_init[] = {3.0374E-05, 6.9969E+00, -8.3962E+01};                               
-double seq_tsr_ctr_mdl_inr_init[] = {-1.0012E-05, 2.8040E-10, 4.0944E-11};                               
-double seq_tsr_ctr_mdl_off_init[] = {2.5413E-04, 1.5889E-10, 9.6735E-12};                                
+double blres_mdl_init[] = {3.7870E-05, 1.2061E-08};
+double dgtog_res_mdl_init[] = {2.9484E-04, -3.7854E-05, 1.7446E-09};
 double alltoall_mdl_init[] = {1.0000E-06, 1.0000E-06, 5.0000E-10};
-double alltoallv_mdl_init[] = {-5.4974E-06, -1.4211E-05, 6.3309E-10};
-double allred_mdl_init[] = {2.1127E-05, 2.7072E-06, 4.2328E-09};                            
-double bcast_mdl_init[] = {6.7536E-05, 3.3990E-06, 2.4632E-09};
+double alltoallv_mdl_init[] = {-7.2507E-06, 1.0362E-05, 1.6246E-10};
+double allred_mdl_init[] = {-3.0267E-06, 7.0181E-06, 3.8877E-09};
+double bcast_mdl_init[] = {-3.5337E-07, 2.5071E-05, 1.5186E-09};
+double long_contig_transp_mdl_init[] = {4.2322E-06, 1.3565E-09};
+double shrt_contig_transp_mdl_init[] = {5.7366E-06, 2.0807E-09};
+double non_contig_transp_mdl_init[] = {5.2266E-06, 2.9382E-09};
+double seq_tsr_ctr_mdl_cst_init[] = {5.3745E-06, 3.6464E-08, 2.2334E-10};
+double seq_tsr_ctr_mdl_ref_init[] = {5.3745E-06, 3.6464E-08, 2.2334E-10};
+double seq_tsr_ctr_mdl_inr_init[] = {5.3745E-06, 3.6464E-11, 8.2334E-12};
+double seq_tsr_ctr_mdl_off_init[] = {2.5413E-04, 1.5889E-10, 9.6735E-12};
 }
 
