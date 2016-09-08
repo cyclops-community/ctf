4be7230d70497ad178d5b801c8faaf1b8e9a9072
Edgar Solomonik <solomonik@inf.ethz.ch>
2016-09-08 13:43:36 -0500

    now really doing something sensible in algebraic multigrid

--- examples/algebraic_multigrid.cxx
+++ examples/algebraic_multigrid.cxx
@@ -7,51 +7,65 @@
 #include <ctf.hpp>
 using namespace CTF;
 
-void smooth_jacobi(Matrix<> & A, Vector<> & x, Vector <> & b){
+void smooth_jacobi(Matrix<> & A, Vector<> & x, Vector <> & b, int nsmooth){
   Vector<> d(x.len, *x.wrld);
   d["i"] = A["ii"];
   Transform<>([](double & d){ d= fabs(d) > 0.0 ? 1./d : 0.0; })(d["i"]);
   Matrix<> R(A);
   R["ii"] = 0.0;
 
-  for (int i=0; i<20; i++){
+  //20 iterations of Jacobi, should probably be a parameter or some convergence check instead
+  for (int i=0; i<nsmooth; i++){
     x["i"] = -1.0*R["ij"]*x["j"];
     x["i"] += b["i"];
     x["i"] *= d["i"];
   }
 }
 
-void vcycle(Matrix<> & A, Vector<> & x, Vector<> & b, Matrix<> * T, int n, int nlevel){
-  smooth_jacobi(A,x,b);
+void vcycle(Matrix<> & A, Vector<> & x, Vector<> & b, Matrix<> * T, int n, int nlevel, int nsmooth){
+  //do smoothing using Jacobi
   Vector<> r(b);
   r["i"] -= A["ij"]*x["j"];
+  double rnorm0 = r.norm2();
+  smooth_jacobi(A,x,b,nsmooth);
+  r["i"] = b["i"];
+  r["i"] -= A["ij"]*x["j"];
   double rnorm = r.norm2();
-  if (A.wrld->rank == 0) printf("residual norm is %E after initial smooth at level %d\n",rnorm,nlevel);
-  if (nlevel == 0) return; 
+  if (nlevel == 0){
+    if (A.wrld->rank == 0) printf("At level %d (coarsest level), residual norm was %1.2E initially\n",nlevel,rnorm0);
+    if (A.wrld->rank == 0) printf("At level %d (coarsest level), residual norm was %1.2E after smooth\n",nlevel,rnorm);
+    return; 
+  }
+  Vector<> x2(x);
+  Vector<> r2(x);
+
+  smooth_jacobi(A,x2,b,nsmooth);
+  r2["i"] = b["i"];
+  r2["i"] -= A["ij"]*x2["j"];
+  double rnorm_alt = r2.norm2();
+  
+
   int m = T[0].lens[1];
 
-  Matrix<> P(T[0].lens[0], T[0].lens[1], SP, *T[0].wrld);
-
-  Vector<> d(x.len, *x.wrld);
+//  Vector<> d(x.len, *x.wrld);
 //  d["i"] = A["ii"];
 //  P["ik"] -= d["i"]*A["ij"]*T[0]["jk"];
+  //smooth the restriction/interpolation operator P = (I-omega*diag(A)^{-1}*A)T
+  Matrix<> P(T[0].lens[0], T[0].lens[1], SP, *T[0].wrld);
   Matrix<> D(n,n,SP,*A.wrld);
-  Matrix<> P1(T[0].lens[0], T[0].lens[1], SP, *T[0].wrld);
   D["ii"] = A["ii"];
   double omega=.1;
   Transform<>([=](double & d){ d= omega/d; })(D["ii"]);
-  P1["ik"] = A["ij"]*T[0]["jk"];
-  P["ik"] -=  D["il"]*P1["lk"];
+  P["ik"] = A["ij"]*T[0]["jk"];
+  P["ik"] =  D["il"]*P["lk"];
   P["ij"] += T[0]["ij"];
 
-
+  //restrict residual vector
   Vector<> PTr(m, *x.wrld);
-  Vector<> zb(m, *b.wrld);
-
-
   PTr["i"] = P["ji"]*r["j"];
-//  PTb["i"] = P["ji"]*b["j"];
-
+ 
+  //coarses initial guess should be zeros
+  Vector<> zx(m, *b.wrld);
   
   int atr = 0;
   if (A.is_sparse){ 
@@ -60,19 +74,32 @@ void vcycle(Matrix<> & A, Vector<> & x, Vector<> & b, Matrix<> * T, int n, int n
   //atr = atr | A.symm;
   Matrix<> AP(n, m, atr, *x.wrld);
   Matrix<> PTAP(m, m, atr, *x.wrld);
-  
+ 
+  //restrict A via triple matrix product, should probably be done outside v-cycle
   AP["lj"] = A["lk"]*P["kj"];
   PTAP["ij"] = P["li"]*AP["lj"];
  
-  vcycle(PTAP, PTr, zb, T+1, m, nlevel-1);
+  //recurse into coarser level
+  vcycle(PTAP, zx, PTr, T+1, m, nlevel-1, nsmooth);
   
-  x["i"] += P["ij"]*PTr["j"]; 
+//  zx.print();
+
+  //interpolate solution to residual equation at coraser level back
+  x["i"] += P["ij"]*zx["j"]; 
  
-  smooth_jacobi(A,x,b);
+  //smooth new solution
   r["i"] = b["i"];
   r["i"] -= A["ij"]*x["j"];
   double rnorm2 = r.norm2();
-  if (A.wrld->rank == 0) printf("residual norm is %E after final smooth at level %d\n",rnorm2,nlevel);
+  smooth_jacobi(A,x,b,nsmooth);
+  r["i"] = b["i"];
+  r["i"] -= A["ij"]*x["j"];
+  double rnorm3 = r.norm2();
+    if (A.wrld->rank == 0) printf("At level %d, residual norm was %1.2E initially\n",nlevel,rnorm0);
+  if (A.wrld->rank == 0) printf("At level %d, residual norm was %1.2E after initial smooth\n",nlevel,rnorm);
+  if (A.wrld->rank == 0) printf("At level %d, residual norm was %1.2E after coarse recursion\n",nlevel,rnorm2);
+  if (A.wrld->rank == 0) printf("At level %d, residual norm was %1.2E after final smooth\n",nlevel,rnorm3);
+  if (A.wrld->rank == 0) printf("At level %d, residual norm was %1.2E if we skipped the coarsening\n",nlevel,rnorm_alt);
 }
 
 /**
@@ -82,6 +109,7 @@ int algebraic_multigrid(int     n,
                         double  sp_frac,
                         int     nlvl,
                         int     ndiv,
+                        int     nsmooth,
                         int     decay_exp,
                         World & dw){
 
@@ -134,9 +162,6 @@ int algebraic_multigrid(int     n,
   
   A["ij"] = Function< std::pair<double,int>, double >([](std::pair<double,int> p){ return p.first; })(B["ij"]);
 
-//  A.print();
-
-
   Vector<> r(b);
   r["i"] -= A["ij"]*x["j"];
   double err = r.norm2(); 
@@ -153,18 +178,16 @@ int algebraic_multigrid(int     n,
       }
     }
     T[i].write(pairs.size(), &(pairs[0]));
-//    T[i].print();
     m = m2;
   }
 
-  vcycle(A, x, b, T, n, nlvl);
+  vcycle(A, x, b, T, n, nlvl, nsmooth);
 
   delete [] T;
   
   r["i"]  = b["i"];
   r["i"] -= A["ij"]*x["j"];
   double err2 = r.norm2(); 
-
  
   bool pass = err2 < err;
 
@@ -194,7 +217,7 @@ char* getCmdOption(char ** begin,
 
 
 int main(int argc, char ** argv){
-  int rank, np, n, pass, nlvl, ndiv, decay_exp;
+  int rank, np, n, pass, nlvl, ndiv, decay_exp, nsmooth;
   double sp_frac;
   int const in_num = argc;
   char ** input_str = argv;
@@ -218,6 +241,11 @@ int main(int argc, char ** argv){
     if (ndiv < 0) ndiv = 2;
   } else ndiv = 2;
 
+  if (getCmdOption(input_str, input_str+in_num, "-nsmooth")){
+    nsmooth = atoi(getCmdOption(input_str, input_str+in_num, "-nsmooth"));
+    if (nsmooth < 0) nsmooth = 3;
+  } else nsmooth = 3;
+
   if (getCmdOption(input_str, input_str+in_num, "-decay_exp")){
     decay_exp = atoi(getCmdOption(input_str, input_str+in_num, "-decay_exp"));
     if (decay_exp < 0) decay_exp = 3;
@@ -237,9 +265,9 @@ int main(int argc, char ** argv){
     World dw(argc, argv);
 
     if (rank == 0){
-      printf("Running algebraic smoothed multigrid method with %d levels with divisor %d in V-cycle, %d elements, decayed based on 3D indexing with decay exponent of %d\n",nlvl,ndiv,n,decay_exp);
+      printf("Running algebraic smoothed multigrid method with %d levels with divisor %d in V-cycle, %d elements, %d smooth iterations, decayed based on 3D indexing with decay exponent of %d\n",nlvl,ndiv,n,nsmooth, decay_exp);
     }
-    pass = algebraic_multigrid(n, sp_frac, nlvl, ndiv, decay_exp, dw);
+    pass = algebraic_multigrid(n, sp_frac, nlvl, ndiv, nsmooth, decay_exp, dw);
     assert(pass);
   }
 
