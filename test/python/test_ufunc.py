041333acde035e5f19a133fbd2376cdab200765f
Qiming Sun <osirpt.sun@gmail.com>
2017-01-11 16:29:10 -0800

    Add tests for numpy ufunc type functions

--- test/python/test_ufunc.py
+++ test/python/test_ufunc.py
@@ -9,6 +9,9 @@ ctf.from_nparray = numpy.asarray
 ctf.to_nparray = numpy.asarray
 ctf.astensor = numpy.asarray
 
+def allclose(a, b):
+    return abs(ctf.to_nparray(a) - ctf.to_nparray(b)).sum() < 1e-14
+
 class KnowValues(unittest.TestCase):
     def test_abs(self):
         a0 = numpy.arange(2., 5.)
@@ -50,51 +53,180 @@ class KnowValues(unittest.TestCase):
     def test__mul__(self):
         a0 = numpy.arange(24.).reshape(4,3,2) + .4
         a1 = ctf.astensor(a0)
-        self.assertTrue(ctf.all(a1*.5 == a0*.5))
+        self.assertTrue(allclose(a1*.5, a0*.5))
         a2 = ctf.astensor(a0*.2+1j)
-        self.assertTrue(ctf.all(a1*a2 == a0*(a0*.2+1j)))
-        self.assertTrue(ctf.all(a1*a0 == a0*a0))
+        self.assertTrue(allclose(a1*a2, a0*(a0*.2+1j)))
+        self.assertTrue(allclose(a1*a0, a0*a0))
+        a2 = numpy.arange(6.).reshape(3,2)
+        self.assertTrue(allclose(a1*a2, a0*a2))
+        a0 = ctf.astensor(numpy.arange(4.))
+        a1 = ctf.astensor(numpy.arange(3.))
+        self.assertTrue((a0.reshape(4,1)*a1).shape == (4,3))
+        self.assertTrue((a1*a0.reshape(4,1)).shape == (4,3))
+        self.assertTrue((a1.reshape(1,3)*a0.reshape(4,1)).shape == (4,3))
+        #self.assertTrue((a1.reshape(1,1,3)*a0.reshape(4,1)).shape == (1,4,3))
+        #self.assertTrue((a1.reshape(1,1,3)*a0.reshape(4,1,1)).shape == (4,1,3))
 
     def test__add__(self):
         a0 = numpy.arange(24.).reshape(4,3,2) + .4
         a1 = ctf.astensor(a0)
-        self.assertTrue(ctf.all(a1+.5 == a0+.5))
+        self.assertTrue(allclose(a1+.5, a0+.5))
         a2 = ctf.astensor(a0*.2+1j)
-        self.assertTrue(ctf.all(a1+a2 == a0+(a0*.2+1j)))
-        self.assertTrue(ctf.all(a1+a0 == a0+a0))
+        self.assertTrue(allclose(a1+a2, a0+(a0*.2+1j)))
+        self.assertTrue(allclose(a1+a0, a0+a0))
+        a2 = numpy.arange(6.).reshape(3,2)
+        self.assertTrue(allclose(a1+a2, a0+a2))
+        a0 = ctf.astensor(numpy.arange(4.))
+        a1 = ctf.astensor(numpy.arange(3.))
+        self.assertTrue((a0.reshape(4,1)+a1).shape == (4,3))
+        self.assertTrue((a1+a0.reshape(4,1)).shape == (4,3))
+        self.assertTrue((a1.reshape(1,3)+a0.reshape(4,1)).shape == (4,3))
+        #self.assertTrue((a1.reshape(1,1,3)+a0.reshape(4,1)).shape == (1,4,3))
+        #self.assertTrue((a1.reshape(1,1,3)+a0.reshape(4,1,1)).shape == (4,1,3))
 
     def test__sub__(self):
         a0 = numpy.arange(24.).reshape(4,3,2) + .4
         a1 = ctf.astensor(a0)
-        self.assertTrue(ctf.all(a1-.5 == a0-.5))
+        self.assertTrue(allclose(a1-.5, a0-.5))
         a2 = ctf.astensor(a0*.2+1j)
-        self.assertTrue(ctf.all(a1-a2 == a0-(a0*.2+1j)))
-        self.assertTrue(ctf.all(a1-a0 == a0-a0))
+        self.assertTrue(allclose(a1-a2, a0-(a0*.2+1j)))
+        self.assertTrue(allclose(a1-a0, a0-a0))
 
     def test__div__(self):
         a0 = numpy.arange(24.).reshape(4,3,2) + .4
         a1 = ctf.astensor(a0)
-        self.assertTrue(ctf.all(a1/.5 == a0/.5))
+        self.assertTrue(allclose(a1/.5, a0/.5))
         a2 = ctf.astensor(a0*.2+1j)
-        self.assertTrue(ctf.all(a1/a2 == a0/(a0*.2+1j)))
-        self.assertTrue(ctf.all(a1/a0 == a0/a0))
+        self.assertTrue(allclose(a1/a2, a0/(a0*.2+1j)))
+        self.assertTrue(allclose(a1/a0, a0/a0))
 
     def test_power(self):
         a0 = numpy.arange(24.).reshape(4,3,2) + .4
         a1 = ctf.astensor(a0)
-        self.assertTrue(ctf.all(ctf.power(a1, .5) == a0**.5))
+        self.assertTrue(allclose(ctf.power(a1, .5), a0**.5))
         a2 = ctf.astensor(a0*.2+1j)
-        self.assertTrue(ctf.all(ctf.power(a1, a2) == a0**(a0*.2+1j)))
-        self.assertTrue(ctf.all(ctf.power(a1, a0) == a0**a0))
+        self.assertTrue(allclose(ctf.power(a1, a2), a0**(a0*.2+1j)))
+        self.assertTrue(allclose(ctf.power(a1, a0), a0**a0))
 
     def test__pow__(self):
         a0 = numpy.arange(24.).reshape(4,3,2) + .4
         a1 = ctf.astensor(a0)
-        self.assertTrue(ctf.all(a1**.5 == a0**.5))
+        self.assertTrue(allclose(a1**.5, a0**.5))
         a2 = ctf.astensor(a0*.2+1j)
-        self.assertTrue(ctf.all(a1**a2 == a0**(a0*.2+1j)))
-        self.assertTrue(ctf.all(a1**a0 == a0**a0))
-        self.assertTrue(ctf.all(a0**a1 == a0**a0))
+        self.assertTrue(allclose(a1**a2, a0**(a0*.2+1j)))
+        self.assertTrue(allclose(a1**a0, a0**a0))
+
+    def test__rmul__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0)
+        self.assertTrue(allclose(.5*a1, a0*.5))
+        a2 = ctf.astensor(a0*.2+1j)
+        self.assertTrue(allclose(a0*a2, a0*(a0*.2+1j)))
+        a2 = numpy.arange(6.).reshape(3,2)
+        self.assertTrue(allclose(a0*ctf.astensor(a2), a0*a2))
+        a0 = numpy.arange(3.)
+        a1 = ctf.astensor(numpy.arange(4.))
+        self.assertTrue((a0.reshape(3,1)*a1).shape == (3,4))
+        self.assertTrue((a0*a1.reshape(4,1)).shape == (4,3))
+        self.assertTrue((a0.reshape(1,3)*a1.reshape(4,1)).shape == (4,3))
+        #self.assertTrue((a0.reshape(1,1,3)*a1.reshape(4,1)).shape == (1,4,3))
+        #self.assertTrue((a0.reshape(1,1,3)*a1.reshape(4,1,1)).shape == (4,1,3))
+
+    def test__radd__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0)
+        self.assertTrue(allclose(.5+a1, a0+.5))
+        a2 = ctf.astensor(a0*.2+1j)
+        self.assertTrue(allclose(a0+a2, a0+(a0*.2+1j)))
+        a2 = numpy.arange(6.).reshape(3,2)
+        self.assertTrue(allclose(a0+ctf.astensor(a2), a0+a2))
+        a0 = numpy.arange(3.)
+        a1 = ctf.astensor(numpy.arange(4.))
+        self.assertTrue((a0.reshape(3,1)+a1).shape == (3,4))
+        self.assertTrue((a0+a1.reshape(4,1)).shape == (4,3))
+        self.assertTrue((a0.reshape(1,3)+a1.reshape(4,1)).shape == (4,3))
+        #self.assertTrue((a0.reshape(1,1,3)+a1.reshape(4,1)).shape == (1,4,3))
+        #self.assertTrue((a0.reshape(1,1,3)+a1.reshape(4,1,1)).shape == (4,1,3))
+
+    def test__rsub__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0)
+        self.assertTrue(allclose(.5-a1, .5-a0))
+
+    def test__rdiv__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0)
+        self.assertTrue(allclose(.5/a1, .5/a0))
+
+    def test__rpow__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0)
+        self.assertTrue(allclose(.5**a1, .5**a0))
+        self.assertTrue(allclose(a0**a1, a0**a0))
+
+    def test__imul__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        a1 *= .5
+        self.assertTrue(allclose(a1, a0*.5))
+
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        with self.assertRaises(TypeError):
+            a1 *= a0*.2+1j
+
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        a2 = numpy.arange(6.).reshape(3,2)
+        a1 *= a2
+        self.assertTrue(allclose(a1, a0*a2))
+
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        a2 = numpy.arange(2.)
+        a1 *= a2
+        self.assertTrue(allclose(a1, a0*a2))
+
+    def test__iadd__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        a1 += .5
+        self.assertTrue(allclose(a1, a0+.5))
+
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        with self.assertRaises(TypeError):
+            a1 += a0*.2+1j
+
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        a2 = numpy.arange(6.).reshape(3,2)
+        a1 += a2
+        self.assertTrue(allclose(a1, a0+a2))
+
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        a2 = numpy.arange(2.)
+        a1 += a2
+        self.assertTrue(allclose(a1, a0+a2))
+
+    def test__isub__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        a1 -= .5
+        self.assertTrue(allclose(a1, a0-.5))
+
+    def test__idiv__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        a1 /= .5
+        self.assertTrue(allclose(a1, a0/.5))
+
+    def test__ipow__(self):
+        a0 = numpy.arange(24.).reshape(4,3,2) + .4
+        a1 = ctf.astensor(a0).copy()
+        a1 **= .5
+        self.assertTrue(allclose(a1, a0**.5))
 
 
 if __name__ == "__main__":
