7f40f2dfe868a343fd5324c8c2fac5c268441ade
Edgar Solomonik <solomon2@illinois.edu>
2017-06-05 15:30:08 -0500

    added all() function at C++ level

--- src_python/ctf_ext.cxx
+++ src_python/ctf_ext.cxx
@@ -1,6 +1,15 @@
 #include "ctf_ext.h"
 #include "../src/tensor/untyped_tensor.h"
 namespace CTF_int{
+  
+  template <typename dtype>
+  void all(tensor * A, tensor * B_bool, char const * idx_A, char const * idx_B){
+    //CTF::Monoid<bool> m(true, [](bool a, bool b){ return a && b; }, MPI_BAND);
+    //tensor B2(&m, B_bool->order, B_bool->lens, B_bool->sym, B_bool->wrld);
+
+    B_bool->operator[](idx_B) = CTF::Function<dtype,bool>([](dtype a){ return a!=0; })(A->operator[](idx_A));
+    B_bool->operator[](idx_B) = -B_bool->operator[](idx_B);
+  }
 
   int64_t sum_bool_tsr(tensor * A){
     CTF::Scalar<int64_t> s(*A->wrld);
--- src_python/ctf_ext.h
+++ src_python/ctf_ext.h
@@ -2,6 +2,11 @@
 #include "../include/ctf.hpp"
   
 namespace CTF_int{
+
+  template <typename dtype>
+  void all(tensor * A, tensor * B_bool, char const * idx_A, char const * idx_B);
+
+
   /**
    * \brief sum all 1 values in boolean tensor
    * \param[in] A tensor of boolean values
