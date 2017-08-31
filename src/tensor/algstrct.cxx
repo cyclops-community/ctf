98bd14b5e9ad9bbaf65e04f15eac5756b55e850e
Eduardo Yap <ejyap2@illinois.com>
2017-08-31 03:11:07 -0500

    debugging through print statements

--- src/tensor/algstrct.cxx
+++ src/tensor/algstrct.cxx
@@ -72,6 +72,7 @@ namespace CTF_int {
       }
       int group_count[] = {1};
       cout << "dgemm" << endl;
+      cout << "tA: " << taA[0] << ", tB: " << taB[0] << ", m: " << m[0] << ", n: " << n[0] << ", k: " << k[0] << ", alpha: " << alpha[0] << ", beta: " << beta[0] << endl; 
       CTF_BLAS::DGEMM_BATCH(taA, taB, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc, group_count, size_per_group);
     }
   }
@@ -189,6 +190,8 @@ namespace CTF_int {
     } else {
       lda_B = n;
     }
+    cout << "dgemm" << endl;
+    cout << "tA: " << tA << ", tB: " << tB << ", m: " << m << ", n: " << n << ", k: " << k << ", alpha: " << alpha << ", beta: " << beta << endl; 
     CTF_BLAS::DGEMM(&tA,&tB,&m,&n,&k,&alpha,A,&lda,B,&lda_B,&beta,C,&lda_C);
   }
 
