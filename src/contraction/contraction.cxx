872faa8eb319277fd62082935067bc2062010fb1
Yuchen Pang <32781495+yuchenpang@users.noreply.github.com>
2020-04-20 16:26:46 -0500

    change the threshold of exhaustive mapping search for contraction

--- src/contraction/contraction.cxx
+++ src/contraction/contraction.cxx
@@ -3078,7 +3078,7 @@ namespace CTF_int {
     A->set_padding();
     B->set_padding();
     C->set_padding();
-    if (gbest_time_sel < 1.){
+    if (gbest_time_sel < 100.){
       gbest_time_exh = gbest_time_sel+1.;
       ttopo_exh = ttopo_sel;
     } else {
