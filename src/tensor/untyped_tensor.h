4dfe8a1132e1f2f2b10269b4477bbe31fcd5da90
Edgar Solomonik <solomon2@illinois.edu>
2020-07-30 21:04:05 -0500

    fix reshape for tesnors with unit edge lengths

--- src/tensor/untyped_tensor.h
+++ src/tensor/untyped_tensor.h
@@ -1046,6 +1046,12 @@ namespace CTF_int {
        * \return list of tensors as described above
        */
       std::vector<tensor*> partition_last_mode_implicit();
+
+      /**
+       * \brief return alias to tensor with no lengths of size 1
+       * \return tensor with same data point as this one but no edge lengths of size 1
+       */
+      tensor * get_no_unit_len_alias();
   };
 }
 #endif// __UNTYPED_TENSOR_H__
