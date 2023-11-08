1adad4e47aa7134d8ff98252de54bd5fa034961d
Edgar Solomonik <solomon2@illinois.edu>
2023-11-07 20:53:44 -0600

    try reorder imports to fix macos build issue for python

--- src_python/ctf/__init__.py
+++ src_python/ctf/__init__.py
@@ -1,10 +1,10 @@
-from ctf import random
 from ctf.world import *
+from ctf.tensor_aux import *
 from ctf.tensor import *
 from ctf.term import *
-from ctf.tensor_aux import *
 from ctf.profile import *
 from ctf.partition import *
 from ctf.multilinear import *
 from ctf.linalg import *
+from ctf import random
 
